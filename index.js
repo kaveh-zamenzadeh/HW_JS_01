1b59c6a8bd8f282b49db3349b05a35d24d230972
kaveh zamenzadeh <zamenzadehkaveh@gmail.com>
2022-06-26 09:52:25 +0430

    Alert

---
+++ index.js
@@ -0,0 +1,12 @@
+let userName = "";
+const inpot = prompt("Enter your name:");
+userName = inpot;
+console.log(userName);
+if (userName == "kaveh")
+{
+    alert("true");
+}
+else
+{
+    alert("false");    
+}
